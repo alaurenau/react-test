9db31d0fd099c2689d5d86772a989caa1815aa0a
Anton Laurenau <alaurenau@gmail.com>
2020-03-21 22:47:32 +0300

    11 Basic grid

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 const App = () => {
     return (
-        <div className="App">
-            <div>
-                <Header/>
-                <Technologies/>
+        <div className="app-wrapper">
+            <header className='header'>
+                <img src={logo} alt='logo'/>
+            </header>
+            <nav className='nav'>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div>content</div>
+                <div>ava</div>
+                <div>My posts
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
             </div>
         </div>);
 };
